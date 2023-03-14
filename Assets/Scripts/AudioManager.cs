37f30bba413e954053f4ef8e9d4b37f6c872bc52
Troubleeee <120094302+Troubleeee@users.noreply.github.com>
2023-03-14 12:41:29 +0100

    Background Sound Sceneswitch fix
    Even when scenes change, background music continues playing

--- Assets/Scripts/AudioManager.cs
+++ Assets/Scripts/AudioManager.cs
@@ -7,8 +7,22 @@ public class AudioManager : MonoBehaviour
 
     public Sound[] sounds;
 
+    public static AudioManager instance;
+
     void Awake()
     {
+
+        if (instance == null)
+        {
+            instance = this;
+        }
+        else
+        {
+            Destroy(gameObject);
+            return; 
+        }
+        DontDestroyOnLoad(gameObject);
+
         foreach (Sound s in sounds)
         {
             s.source = gameObject.AddComponent<AudioSource>();
@@ -28,6 +42,11 @@ public class AudioManager : MonoBehaviour
     public void Play(string name)
     {
         Sound s = Array.Find(sounds, sound => sound.name == name);
+        if (s == null)
+        {
+            Debug.LogWarning($"Sound: {name} not found.");
+            return;
+        }
         s.source.Play();
     }
 }
