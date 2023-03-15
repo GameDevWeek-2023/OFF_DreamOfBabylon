2ddc29756e6a01adbefe9703bbc2fb36be729974
RufusDL <simon.deutscher@gmx.de>
2023-03-15 21:51:52 +0100

    IngameMenu

--- Assets/Scripts/InGameMenu.cs
+++ Assets/Scripts/InGameMenu.cs
@@ -1,14 +1,43 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.SceneManagement;
 
 public class InGameMenu : MonoBehaviour
 {
+    [SerializeField] GameObject ingameUI;
     // Start is called before the first frame update
     void Start()
     {
         
     }
+
+    void OnEscButton(InputValue value)
+    {
+        ContinueGame();
+    }
+
+    public void ContinueGame()
+    {
+        Debug.Log("OnEscape aufgerufen");
+        if (!ingameUI.activeInHierarchy)
+        {
+
+            ingameUI.SetActive(true);
+            Debug.Log("Menu aufgerufen");
+        }
+        else
+        {
+            ingameUI.SetActive(false);
+        }
+    }
+
+    public void BackToMainMenu()
+    {
+        SceneManager.LoadScene(0);
+    }
+
 
     // Update is called once per frame
     void Update()
