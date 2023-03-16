61a007f5922a821716a4f4ede85f93e7bdcf8cd1
Minela Mujezinovic <minelamujezinovic@sh-1x-104-101.wlan-ng.fh-trier.de>
2023-03-16 18:04:33 +0100

    NilsUpdate Boxgrabbing PressureSwitchBug

--- Assets/Scripts/GrabbingMechanic.cs
+++ Assets/Scripts/GrabbingMechanic.cs
@@ -20,6 +20,7 @@ public class GrabbingMechanic : MonoBehaviour
     private Vector2 direction;
 
     private Rigidbody2D boxrb;
+    private bool isGrabbing=false;
 
     // Start is called before the first frame update
     void Start()
@@ -44,33 +45,30 @@ public class GrabbingMechanic : MonoBehaviour
             direction = Vector2.left;
         }
 
-        RaycastHit2D hitInfo = Physics2D.Raycast(rayPoint.position, direction, rayDistance);
-        if (hitInfo.collider != null && hitInfo.collider.gameObject.layer == layerIndex)
+        if (grabbedObject == null)
         {
-            
-            //grabObject
-            if (value.Get<float>() != 0f && grabbedObject == null)
+            RaycastHit2D hitInfo = Physics2D.Raycast(rayPoint.position, direction, rayDistance);
+            if (hitInfo.collider != null && hitInfo.collider.gameObject.layer == layerIndex)
             {
                 grabbedObject = hitInfo.collider.gameObject;
                 Destroy(grabbedObject.GetComponent<Rigidbody2D>());
                 grabbedObject.transform.position = grabPoint.position;
                 grabbedObject.transform.SetParent(transform);
             }
-            //release Object
-            else if(value.Get<float>() != 0f)
-            {
-                grabbedObject.AddComponent<Rigidbody2D>();
-                Rigidbody2D rb = grabbedObject.GetComponent<Rigidbody2D>();
-                rb.sharedMaterial = new PhysicsMaterial2D("BoxMaterial");
-                rb.mass = 10f;
-                rb.drag = 0.3f;
-                rb.collisionDetectionMode = CollisionDetectionMode2D.Continuous;
-                rb.interpolation = RigidbodyInterpolation2D.Interpolate;
-                rb.freezeRotation = true;
+        }
+        else
+        {
+            grabbedObject.AddComponent<Rigidbody2D>();
+            Rigidbody2D rb = grabbedObject.GetComponent<Rigidbody2D>();
+            rb.sharedMaterial = new PhysicsMaterial2D("BoxMaterial");
+            rb.mass = 10f;
+            rb.drag = 0.3f;
+            rb.collisionDetectionMode = CollisionDetectionMode2D.Continuous;
+            rb.interpolation = RigidbodyInterpolation2D.Interpolate;
+            rb.freezeRotation = true;
                 
-                grabbedObject.transform.SetParent(null);
-                grabbedObject = null;
-            }
+            grabbedObject.transform.SetParent(null);
+            grabbedObject = null;
         }
     }
 }
--- Assets/Scripts/PressureSwitch.cs
+++ Assets/Scripts/PressureSwitch.cs
@@ -1,21 +1,30 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class PressureSwitch : Switch
 {
     int numberColliding = 0;
+    public List<GameObject> onSwitch = new List<GameObject>();
 
+    
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        numberColliding++;
+        //numberColliding++;
+        if(!onSwitch.Contains(collision.gameObject))
+            onSwitch.Add(collision.gameObject);
         TurnOn();
     }
 
     private void OnTriggerExit2D(Collider2D collision)
     {
-        numberColliding--;
-        if(numberColliding == 0)
+        //numberColliding--;
+        if (onSwitch.Contains(collision.gameObject))
+        {
+            onSwitch.Remove(collision.gameObject);
+        }
+        if(onSwitch.Count == 0)
         {
             TurnOff();
         }
